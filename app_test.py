4bb4e7fc3c3ccdf399d8d6aa3ef43ef39542e37a
FaroukDev <fa.elayache1@gmail.com>
2021-03-13 15:58:30 +0100

    add test app and logger

---
+++ app_test.py
@@ -0,0 +1,18 @@
+import unittest
+import testing.postgresql
+import psycopg2
+from app import increment , showData , showCurentId, Flask
+from flask_testing import TestCase
+app = Flask(__name__)
+
+class TestRoutesAnsible(unittest.TestCase):
+    def test_increment(self):
+        self.postgresql = testing.postgresql.Postgresql(port=5000)
+
+    def test_some_json(self):
+        response = self.app.get('/id')
+        print(response.data)
+    
+
+if __name__ == '__main__':
+    unittest.main()
